0b6288b2ae7259a08c5e5408dd79462c2f7d0a30
Sailaja Mende <sailajamende@gmail.com>
2026-07-03 15:03:58 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/careerhub/CareerhubServicesApplication.java
@@ -0,0 +1,13 @@
+package com.careerhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerhubServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerhubServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/careerhub/CareerhubServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerhubServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
